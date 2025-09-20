e6030b83a0ca14e1341c348b4cd174f0e167f8d1
chen4023 <ads4023@naver.com>
2025-09-20 23:02:58 +0900

    feat: button 공통 컴포넌트 생성

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/utils/style";
+import { cva } from "class-variance-authority";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "outline";
+  size?: "sm" | "md" | "lg";
+}
+const buttonVariants = cva(
+  "flex items-center justify-center rounded-md text-sm cursor-pointer font-medium transition-colors",
+  {
+    variants: {
+      variant: {
+        primary: "bg-pink-500 text-gray-50 hover:bg-pink-600",
+        secondary: "bg-pink-secondary text-secondary-foreground hover:bg-secondary/90",
+        outline: "border border-input bg-pink-background hover:bg-accent hover:text-accent-foreground",
+        none: "justify-center text-slate-600 hover:text-slate-900",
+      },
+      size: {
+        sm: "h-9 px-2 py-2",
+        md: "h-10 px-4 py-2",
+        lg: "h-11 px-8 py-6",
+      },
+    },
+  }
+);
+
+export default function Button({ variant = "primary", size = "md", ...props }: ButtonProps) {
+
+  return (
+    <button className={cn(buttonVariants({ variant, size }))} {...props} />
+  );
+}
+
